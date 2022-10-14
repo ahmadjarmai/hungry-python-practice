b59e2e863d1797194e889755d0c58bc3047bb1c9
ahmadjarmai <dptaahmad@gmail.com>
2022-10-14 11:10:06 +0100

    add hungry.py

---
+++ hungry.py
@@ -0,0 +1,9 @@
+def hungry() :
+    print('are you hungry')
+    question =input()
+    if question ==1 :
+        print('eat bead and meat')
+        print('eat banana')
+    else :
+        print('keep coding, coding, coding')
+hungry()
